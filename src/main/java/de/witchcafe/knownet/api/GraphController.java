33053eae23eb7f8a585f40acbe738400526840a0
FrankS <frank.segieth@gmail.com>
2026-08-07 23:26:31 +0200

    graphControler public now

--- src/main/java/de/witchcafe/knownet/api/GraphController.java
+++ src/main/java/de/witchcafe/knownet/api/GraphController.java
@@ -4,12 +4,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import com.vaadin.flow.server.auth.AnonymousAllowed;
 
 import de.witchcafe.knownet.service.GraphService;
 import de.witchcafe.knownet.service.GraphService.GraphData;
 
 @RestController
 @RequestMapping("/api/graph")
+@AnonymousAllowed
 public class GraphController {
 
     private final GraphService graphService;
